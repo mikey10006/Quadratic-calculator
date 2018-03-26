52dcde79775c2a1fac33cd1d7c455dfc763c5bcc
Shemar <shemarjconstable@gmail.com>
2018-03-25 23:30:40 -0400

    Add files via upload

---
+++ simple quadratic calculator.py
@@ -0,0 +1,20 @@
+
+#simple quadratic calculator using quadratic formula
+z=float(input("Enter the coefficient of a: "))
+
+k= float(input("Enter the coefficient of b: "))
+
+s= float(input("Enter the coefficient of c: "))
+print("Please wait a moment while we calculate your shiz..")
+def quad (a,b,c):
+    num1=((b*2)+ (-4*a*c))*0.5
+    num2=-b
+    den=2*a
+    numi=num1+num2
+    numo=num1-num2
+    answer1=numi/den
+    answer2=numo/den
+
+    print("Values of x: ",round(answer1,2), "and", round(answer2,2))
+
+quad(z,k,s)
